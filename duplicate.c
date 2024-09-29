3b53d0f780d9b8a102b4a53da9c7a779cc77786c
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2024-09-29 22:33:44 +0530

    Update duplicate.c

--- duplicate.c
+++ duplicate.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 
-int n;
-int arr[10];
+int findDuplicate(int* num, int numSize) {
+    int count[numSize];
+    for (int i = 0; i < numSize; i++) {
+        count[i] = 0;
+    }
 
-int main()
-{
+    for (int i = 0; i < numSize; i++) {
+        count[num[i]]++;
+        if (count[num[i]] > 1) {
+            return num[i];
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int num[] = {1, 3, 4, 2, 2};
+    int size = sizeof(num) / sizeof(num[0]);
+    
+    int duplicate = findDuplicate(num, size);
+    printf("The duplicate number is: %d\n", duplicate);
 
     return 0;
 }
