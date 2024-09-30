710d56857c4417369807e965592992332e14301e
Sahil-Sinha-11 <sahilsinha6969@gmail.com>
2024-10-01 01:01:39 +0530

    coded binary search algorithm

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -1,18 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void binary_search()
 
 
-void maxElement(vector<int>& array, int n)
-{
-    int max_element=array[0];
-
-    for(int i=1;i<=n;i++)
-      if(max_element<array[i])
-        max_element=array[i];
-    
-     cout<<"Maximum Element in the array is : "<<max_element;
-}
 
 void printVector(const vector<int>& array)
 {
@@ -21,20 +12,20 @@ void printVector(const vector<int>& array)
 }
 
 
+
 int main() {
-    int numberOfElements;
+    int numberOfElements,k;
     cout << "Enter the number of elements in the array: ";
     cin >> numberOfElements;
     vector<int> array(numberOfElements);
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < numberOfElements; i++) cin >> array[i];
-    cout<< "Given vector is: ";
+    cout<< "Given array is: ";
     printVector(array);
-    maxElement(array, numberOfElements - 1);
+    cout<<"enter the no. to be found";
+    cin>>k;
+    binary_search(array,k);
     
     return 0;
 
 }
-
-
-
