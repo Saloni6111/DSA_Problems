fe998e28c4e93e900390f749e6911bc4d766a1df
Prax860 <gprakhar860@gmail.com>
2024-10-01 14:27:11 +0530

    Median_of_2_sorted-arrays(Prax860)

--- C++/Median_2_sorted-arrays.cpp
+++ C++/Median_2_sorted-arrays.cpp
@@ -52,3 +52,16 @@ int main() {
 
     return 0;
 }
+
+
+
+// Example 1:
+
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+// Explanation: merged array = [1,2,3] and median is 2.
+// Example 2:
+
+// Input: nums1 = [1,2], nums2 = [3,4]
+// Output: 2.50000
+// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
