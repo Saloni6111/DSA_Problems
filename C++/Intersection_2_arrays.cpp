c634790bd3b31505b0a0e37b50fdd8e8d2a8178d
Bilal <39002942+byhlel@users.noreply.github.com>
2024-10-01 05:34:19 +0000

    feat: added common and tricky tests

--- C++/Intersection_2_arrays.cpp
+++ C++/Intersection_2_arrays.cpp
@@ -6,7 +6,10 @@ using namespace std;
 
 // Function to find the intersection of two arrays
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    // Set allows to discard duplicates
     unordered_set<int> set1(nums1.begin(), nums1.end());
+    // Ensure we don't add duplicates
+    // Could potentially create set for nums2 and directly insert into a vector
     unordered_set<int> resultSet;
     for (int num : nums2) {
         if (set1.find(num) != set1.end()) {
@@ -18,12 +21,27 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 
 // Test case
 int main() {
-    vector<int> nums1 = {1, 2, 2, 1};
-    vector<int> nums2 = {2, 2};
+    // Change vectors for your own test cases
+    vector<int> nums1 = {1,2,2,1};
+    vector<int> nums2 = {2,2};
     vector<int> result = intersection(nums1, nums2);
-    cout << "Intersection: ";
-    for (int num : result) {
-        cout << num << " ";
+    if (!result.empty())
+    {
+        cout << "Intersection: ";
+        for (int num : result) {
+            cout << num << " ";
+        }
+    }
+    else
+    {
+        cout << "No intersection";
     }
     return 0;
 }
+
+// Test case 1: nums1:{1, 2, 2, 1}, nums2:{2, 2} 
+// Expected output: [1, 2]
+
+
+// Test case 2: nums1:{}, nums2:{} 
+// Expected output: [] or No intersection
