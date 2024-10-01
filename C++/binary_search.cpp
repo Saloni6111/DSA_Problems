7167526fcf43a340e08fb35fab78e67a46333479
Omji-Kesharwani <omjikesharwani000@gamil.com>
2024-10-01 18:43:29 +0530

    Bianry Search

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void binary_search()
+void binary_search(vector<int>& array,int k)
+{
+ int low=0,high=array.size()-1;
+ while(low<=high)
+ {
+    int mid=(high+low)>>1;
+    if(array[mid]==k)
+    {
+        cout<<"Element is found in the array"<<endl;
+        return;
+    }
+    else if(array[mid]>k)
+    {
+        high=mid-1;
+    }
+    else {
+        low=mid+1;
+    }
+
+ }
+ cout<<"Element is not found in the given array."<<endl;
+}
 
 
 
@@ -24,6 +45,7 @@ int main() {
     printVector(array);
     cout<<"enter the no. to be found";
     cin>>k;
+    sort(array.begin(),array.end());
     binary_search(array,k);
     
     return 0;
