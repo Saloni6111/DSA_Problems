10947f1af0c2c37f9fc70b87fbe9da4f91d22ae8
Utkarsh-Aggarwal <utkarshaggarwal06@gmail.com>
2024-10-02 15:14:11 +0530

    Sorted-Linked_list_added

---
+++ C++/Sorted_linkedlist_BST.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct LNode {
+    int data;
+    struct LNode* next;
+
+    LNode(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct TNode {
+    int data;
+    struct TNode* left;
+    struct TNode* right;
+
+    TNode(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void preOrder(TNode* node) {
+    if (node == NULL)
+        return;
+    cout << node->data << " ";
+    preOrder(node->left);
+    preOrder(node->right);
+}
+
+
+// } Driver Code Ends
+/*
+struct LNode { //Linked List
+    int data;
+    struct LNode* next;
+    LNode(int x){
+        data = x;
+        next = NULL; }
+};
+struct TNode {   //Tree
+    int data;
+    struct TNode* left;
+    struct TNode* right;
+    TNode(int x){
+        data=x;
+        left=right=NULL; }
+}; */
+class Solution {
+  public:
+    TNode *sortedListToBST(LNode *head) {
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        LNode* head = new LNode(data);
+        LNode* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new LNode(data);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        TNode* Thead = ob.sortedListToBST(head);
+        preOrder(Thead);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
