bd5d7c3ddfb00b80a7a327011b384e1545c84e97
Brijeshthummar02 <brijeshthummar02@gmail.com>
2024-10-02 13:44:06 +0530

    Conflict solved

--- Java/SpiralMatrix.java
+++ Java/SpiralMatrix.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SpiralMatrix {
+
+    // Method to print the matrix in spiral order (original from first version)
     public static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
         if (matrix == null || matrix.length == 0) return result;
@@ -44,7 +47,80 @@ public class SpiralMatrix {
         return result;
     }
 
+    // Method to create a spiral pattern of '*' and '.' (from second version)
+    public static void printSpiralPattern(int n) {
+        char[][] matrix = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = '.';
+            }
+        }
+
+        if (n % 2 == 0) {
+            int l = 0, r = n - 1, t = 0, d = n - 1;
+            int i = 0, j = 0;
+            while (l < n / 2) {
+                while (j < r) {
+                    matrix[i][j] = '*';
+                    j++;
+                }
+                while (i < d) {
+                    matrix[i][j] = '*';
+                    i++;
+                }
+                while (j > l) {
+                    matrix[i][j] = '*';
+                    j--;
+                }
+                while (i > t) {
+                    matrix[i][j] = '*';
+                    i--;
+                }
+                l += 2;
+                t += 2;
+                r -= 2;
+                d -= 2;
+                i = t;
+                j = l;
+            }
+        } else {
+            int l = 0, r = n - 1, t = 0, d = n - 1;
+            int i = 0, j = 0;
+            while (l < (n + 1) / 2) {
+                while (j < r) {
+                    matrix[i][j] = '*';
+                    j++;
+                }
+                while (i < d) {
+                    matrix[i][j] = '*';
+                    i++;
+                }
+                while (j > l) {
+                    matrix[i][j] = '*';
+                    j--;
+                }
+                t += 2;
+                while (i > t) {
+                    matrix[i][j] = '*';
+                    i--;
+                }
+                l += 2;
+                r -= 2;
+                d -= 2;
+            }
+            matrix[i][j] = '*';
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
+        // Example for spiralOrder function (first version)
         int[][] matrix = {
             {1, 2, 3},
             {4, 5, 6},
@@ -52,6 +128,13 @@ public class SpiralMatrix {
         };
 
         List<Integer> result = spiralOrder(matrix);
-        System.out.println(result);
+        System.out.println("Spiral order of the matrix: " + result);
+
+        // Example for printSpiralPattern function (second version)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of matrix for spiral pattern:");
+        int n = sc.nextInt();
+        printSpiralPattern(n);
+        sc.close();
     }
 }
