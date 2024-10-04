c45c9c96b271163ce482b6dcb9c7d02859df5f8b
chaanakyaaM <chaanakyaam@gmail.com>
2024-10-04 21:30:58 +0530

    solution for median of two sorted arrays is added

---
+++ Python/Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Merge the arrays into a single sorted array.
+        merged = nums1 + nums2
+
+        # Sort the merged array.
+        merged.sort()
+
+        # Calculate the total number of elements in the merged array.
+        total = len(merged)
+
+        if total % 2 == 1:
+            # If the total number of elements is odd, return the middle element as the median.
+            return float(merged[total // 2])
+        else:
+            # If the total number of elements is even, calculate the average of the two middle elements as the median.
+            middle1 = merged[total // 2 - 1]
+            middle2 = merged[total // 2]
+            return (float(middle1) + float(middle2)) / 2.0
