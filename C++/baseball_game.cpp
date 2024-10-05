801c037ff4c46b72f8810ee29771efc88de8b389
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-10-05 13:43:55 +0530

    First commit

--- C++/baseball_game.cpp
+++ C++/baseball_game.cpp
@@ -12,14 +12,18 @@ public:
             if(operations[i] != "D" && operations[i] != "C" && operations[i] != "+"){
 
                 record.push_back(stoi(operations[i]));
+                // We need to push the scores in the form of integers, and stoi function helps us convert string to an integer
             }
             else if(operations[i] == "+"){
 
                 record.push_back(record[record.size()-1] + record[record.size()-2]);
+                // size of the record array could be smaller than the index, so each time we want to access elements from last of the array
+                // we will use record.size() for that purpose!!
             }
             else if(operations[i] == "C"){
 
                 record.pop_back();
+                
             }
             else if(operations[i] == "D"){
 
@@ -28,12 +32,14 @@ public:
         }
 
         int sum = 0;
+        // sum variable will be storing the sum of all records
 
         for(int i = 0; i < record.size(); i++){
 
             sum += record[i];
 
         }
+        
         return sum;
         
     }
